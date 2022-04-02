c1fbcbf2160380b1c1e1df0a9b4552c6b93f7cfa
Kornel Koszela <kornel.koszela@gmail.com>
2022-04-02 23:55:32 +0200

    Start work on websocket JWT authentication

---
+++ geodata/utils.py
@@ -0,0 +1,23 @@
+from rest_framework_simplejwt.authentication import JWTAuthentication
+from urllib.parse import urlparse
+from channels.db import database_sync_to_async
+
+
+def get_user(token):
+    authenticator = JWTAuthentication()
+    payload = authenticator.get_validated_token(token)
+    return payload["user"]
+
+
+class JWTAuthMiddleware:
+    """
+    Custom middleware to process JWT tokens from query strings for ASGI requests.
+    """
+
+    def __init__(self, app: object):
+        self.app = app
+
+    def __call__(self, scope, receive, send):
+        print("JWTAuthMiddleware __call__")
+        params = urlparse.parse_qs(scope["query_string"])
+        scope["user"] = get_user(params.get("token", [None])[0])
