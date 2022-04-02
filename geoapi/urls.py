4af8d820ea187451fdce1c7b07ee525774b89a84
Kornel <kornel@rustyforge.con>
2022-04-02 11:25:12 +0200

    Default to docs

--- geoapi/urls.py
+++ geoapi/urls.py
@@ -45,12 +45,12 @@ urlpatterns = [
     ),
     path("api/schema/", SpectacularAPIView.as_view(), name="schema"),
     path(
-        "api/schema/swagger-ui/",
+        "",
         SpectacularSwaggerView.as_view(url_name="schema"),
         name="swagger-ui",
     ),
     path(
-        "api/schema/redoc/",
+        "/redoc/",
         SpectacularRedocView.as_view(url_name="schema"),
         name="redoc",
     ),
