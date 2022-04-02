7eacbdcb203ecc993ad56df94606f671cbf897f5
Kornel Koszela <kornel.koszela@gmail.com>
2022-04-02 09:17:28 +0200

    Basic view and serializer

--- geodata/views.py
+++ geodata/views.py
@@ -1,3 +1,28 @@
-from django.shortcuts import render
+from rest_framework import viewsets
+from rest_framework.response import Response
+from rest_framework.decorators import action
+from rest_framework.permissions import IsAuthenticated
 
-# Create your views here.
+from geodata.models import GeoData
+from geodata.serializers import GeoDataSerializer
+
+
+class GeoDataViewSet(viewsets.ModelViewSet):
+    serializer_class = GeoDataSerializer
+    permission_classes = (IsAuthenticated,)
+    allowed_methods = ("GET", "POST", "PUT", "DELETE")
+    lookup_field = "slug"
+    filterset_fields = (
+        "address",
+        "continent_name",
+        "country_name",
+        "region_name",
+        "city",
+        "latitude",
+        "longitude",
+        "task_id",
+        "timestamp",
+    )
+
+    def get_queryset(self):
+        return GeoData.objects.filter(user=self.request.user)
