88be8c604a4fa5c8c92915c39089e99a1fd1ef74
Kornel Koszela <kornel.koszela@gmail.com>
2022-04-02 09:17:51 +0200

    API and docs

--- geoapi/urls.py
+++ geoapi/urls.py
@@ -14,8 +14,36 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include
+from rest_framework import routers
+from drf_yasg.views import get_schema_view
+from drf_yasg import openapi
+
+from geodata.views import GeoDataViewSet
+
+schema_view = get_schema_view(
+    openapi.Info(
+        title="GeoData API",
+        default_version="v1",
+        description="GeoData API",
+        terms_of_service="https://www.google.com/policies/terms/",
+        contact=openapi.Contact(email="kornel.koszela@gmail.com"),
+        license=openapi.License(name="BSD License"),
+        validators=["ssv"],
+    ),
+    public=True,
+)
+
+router = routers.DefaultRouter()
+router.register(r"geodata", GeoDataViewSet, basename="geodata")
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
+    path("admin/", admin.site.urls),
+    path("api/v1/", include(router.urls)),
+    path(
+        "swagger/",
+        schema_view.with_ui("swagger", cache_timeout=0),
+        name="schema-swagger-ui",
+    ),
+    path("redoc/", schema_view.with_ui("redoc", cache_timeout=0), name="schema-redoc"),
 ]
