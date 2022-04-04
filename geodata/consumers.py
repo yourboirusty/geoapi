4d92f568d21601f36cc92df270f1a8110a85dd9b
Kornel <kornel@rustyforge.con>
2022-04-04 16:48:09 +0200

    Clean disconnect

--- geodata/consumers.py
+++ geodata/consumers.py
@@ -60,22 +60,28 @@ class WorkerResponseConsumer(StackedAsyncJsonWebsocketConsumer):
         if not (task_id := self.scope["url_route"]["kwargs"].get("task_id")):
             await self.close(code=400)
         await self.accept()
+        self.task_id = task_id
         self._append_runstack(
-            self.send(text_data=f"Connected to task {task_id}")
+            self.send(text_data=f"Connected to task {self.task_id}")
         )
         self._append_runstack(
             self.channel_layer.group_add(  # type: ignore
-                task_id,
+                self.task_id,
                 self.channel_name,
             )
         )
-        process_result = process_geodata.AsyncResult(task_id)  # type: ignore
+        task_result = process_geodata.AsyncResult(self.task_id)  # type: ignore
         self.send_status(
-            status=process_result.status, response=process_result.result
+            status=task_result.status, response=task_result.result
         )
 
     async def disconnect(self, close_code):
-        pass
+        await self._await_stack()
+        self._clean_stack()
+        self.channel_layer.group_discard(  # type: ignore
+            self.task_id,
+            self.channel_name,
+        )
 
     def send_status(
         self, status: str, response: Optional[str] = None, close=False
