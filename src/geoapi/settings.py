2414f127b15cb18a7c5e82c1ca327e4fa0fb0422
Kornel Koszela <kornel.koszela@gmail.com>
2022-04-07 17:32:02 +0200

    heroku/fix-swagger

--- src/geoapi/settings.py
+++ src/geoapi/settings.py
@@ -170,6 +170,8 @@ STATIC_URL = "/static/"
 
 STATIC_ROOT = os.path.join(BASE_DIR, "static")
 
+STATICFILES_STORAGE = "whitenoise.storage.CompressedManifestStaticFilesStorage"
+
 # Default primary key field type
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
 
