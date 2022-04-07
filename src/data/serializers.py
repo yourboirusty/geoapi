4de500ecab2a1b1e7904cc89f6d29bc342c1ea86
Kornel Koszela <kornel.koszela@gmail.com>
2022-04-07 19:16:24 +0200

    add-address-validation/validation

--- src/data/serializers.py
+++ src/data/serializers.py
@@ -1,5 +1,6 @@
 from rest_framework import serializers
 from data.models import GeoData
+from validators import domain, ip_address
 
 
 class GeoDataSerializer(serializers.ModelSerializer):
@@ -20,3 +21,14 @@ class GeoDataSerializer(serializers.ModelSerializer):
         read_only_fields = ("slug", "timestamp", "address")
 
         lookup_field = "slug"
+
+
+class AddressLookupSerializer(serializers.Serializer):
+    lookup_address = serializers.CharField(max_length=100)
+
+    def validate_lookup_address(self, value):
+        if not (
+            domain(value) or ip_address.ipv4(value) or ip_address.ipv6(value)
+        ):
+            raise serializers.ValidationError("Invalid lookup address")
+        return value
--- src/data/websocket/serializers.py
+++ src/data/websocket/serializers.py
@@ -1,21 +1,9 @@
 from rest_framework import serializers
-from validators import domain, ip_address
 
 
 class WorkerStatusResponseSerializer(serializers.Serializer):
     status = serializers.CharField(max_length=100)
     response = serializers.CharField(max_length=100, allow_blank=True)
-
-
-class AddressLookupSerializer(serializers.Serializer):
-    lookup_address = serializers.CharField(max_length=100)
-
-    def validate_lookup_address(self, value):
-        if not (
-            domain(value) or ip_address.ipv4(value) or ip_address.ipv6(value)
-        ):
-            raise serializers.ValidationError("Invalid lookup address")
-        return value
 
 
 class AddressLookupResponseSerializer(serializers.Serializer):
