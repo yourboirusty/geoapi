0533e9a0f22e702e467f4bb0ce12161dbeb7b90e
Kornel <kornel@rustyforge.con>
2022-04-07 12:11:44 +0200

    docker/create-image

--- src/geoapi/__init__.py
+++ src/geoapi/__init__.py
@@ -1,3 +1,5 @@
+from .wsgi import application as wsgi_app
+from .asgi import application as asgi_app
 from .celery import app as celery_app
 
-__all__ = ("celery_app",)
+__all__ = ("wsgi_app", "asgi_app", "celery_app")
--- src/geoapi/asgi.py
+++ src/geoapi/asgi.py
@@ -13,7 +13,6 @@ from channels.routing import ProtocolTypeRouter, URLRouter
 from channels.auth import AuthMiddlewareStack
 from data.websocket.consumers import WorkerResponseConsumer
 
-
 from django.core.asgi import get_asgi_application
 
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "mysite.settings")
